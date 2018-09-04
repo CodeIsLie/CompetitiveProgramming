f1a3091a6fba6a6b7ef72bb42868b5114a6c598b
CodeIsLie <ilya1757@gmail.com>
2018-09-04 09:42:42 +0300

    add train 3 task 4 (cutpoints). Don't pass time test

---
+++ Train1/train3_4.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+vector<vector<int>> graph;
+vector<bool> used;
+vector<int> tin;
+vector<int> fup;
+set<int> cutpoints;
+
+int timer;
+
+void dfs (int v, int p = -1) {
+    used[v] = true;
+    tin[v] = fup[v] = timer++;
+    int children = 0;
+    for (size_t i=0; i<graph[v].size(); ++i) {
+        int to = graph[v][i];
+        if (to == p)  continue;
+        if (used[to])
+            fup[v] = min(fup[v], tin[to]);
+        else {
+            dfs(to, v);
+            fup[v] = min(fup[v], fup[to]);
+            if (fup[to] >= tin[v] && p != -1)
+                cutpoints.insert(v);
+            ++children;
+        }
+    }
+    if (p == -1 && children > 1)
+        cutpoints.insert(v);
+}
+
+int main() {
+    freopen("points.in", "r", stdin);
+    int n, m;
+    cin >> n >> m;
+    graph = vector<vector<int>>(n+1);
+
+    int v, to;
+    for (int i = 0; i < m; ++i){
+        cin >> v >> to;
+        graph[v].push_back(to);
+        graph[to].push_back(v);
+    }
+    fclose(stdin);
+
+    used.resize(n+1);
+    tin.resize(n+1);
+    fup.resize(n+1);
+
+    timer = 0;
+    for (int i=1; i<=n; ++i)
+        used[i] = false;
+    for (int i = 1; i <= n; ++i){
+        if (!used[i])
+            dfs(i);
+    }
+
+    freopen("points.out", "w", stdout);
+    cout << cutpoints.size() << endl;
+    for (int p: cutpoints)
+        cout << p << ' ';
+}
