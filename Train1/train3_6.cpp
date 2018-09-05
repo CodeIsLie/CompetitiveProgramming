c97996f22ddbae611f85f96acc2371004f27fe66
CodeIsLie <ilya1757@gmail.com>
2018-09-05 11:22:04 +0300

    complete 6 task (condensation)

--- Train1/train3_6.cpp
+++ Train1/train3_6.cpp
@@ -9,29 +9,84 @@
 
 using namespace std;
 
+vector<int> order;
+vector<int> used;
+int component_counter;
+set<pair<int, int>> edges;
+
+void dfs(int v, vector<vector<int>> & graph){
+    used[v] = 1;
+    for (int to: graph[v]){
+        if (!used[to]) {
+            dfs(to, graph);
+        }
+    }
+    order.push_back(v);
+}
+
+void topological_sort(int n, vector<vector<int>> & graph){
+    for (int i = 1; i <= n; ++i){
+        if (!used[i]) {
+            dfs(i, graph);
+        }
+    }
+    reverse(order.begin(), order.end());
+}
+
+void dfs2 (int v, vector<vector<int>> & graph) {
+    used[v] = component_counter;
+    for (int to: graph[v])
+        if (!used[to])
+            dfs2(to, graph);
+}
+
+void mark_vertices(vector<vector<int>> & graph){
+    for (int v: order){
+        component_counter++;
+        if (!used[v]){
+            dfs2(v, graph);
+        }
+    }
+}
+
+void find_condensation_edges(vector<vector<int>> & graph){
+    for (int i = 1; i < graph.size(); ++i){
+        for (int to: graph[i]){
+            if (used[to] != used[i])
+                edges.emplace(used[i], used[to]);
+        }
+    }
+}
+
 void condensation(){
     freopen("condense2.in", "r", stdin);
     int n, m;
     scanf("%d%d", &n, &m);
 
+    used.assign(n+1, 0);
     vector<vector<int>> graph(n+1);
+    vector<vector<int>> rev_graph(n+1);
     int v, to;
     for (int i = 0; i < m; ++i){
         scanf("%d%d", &v, &to);
         graph[v].push_back(to);
-        graph[to].push_back(v);
+        rev_graph[to].push_back(v);
     }
 
-    int cntEdges = 0;
-    // 1. find topological order
-    // 2. inverse
-    // 3. dfs, mark all
+    topological_sort(n, rev_graph);
 
-    freopen("condense2.out", "w", stdin);
+    used.assign(n+1, 0);
+    component_counter = 0;
+    mark_vertices(graph);
+
+    find_condensation_edges(graph);
+    int cntEdges = edges.size();
+
+    freopen("condense2.out", "w", stdout);
     printf("%d ", cntEdges);
 }
 
-int main6(){
+int main(){
     condensation();
     return 0;
 }
