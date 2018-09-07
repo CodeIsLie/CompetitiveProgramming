3202970e9e0353f5f81c213d7710d9a99b22c753
CodeIsLie <ilya1757@gmail.com>
2018-09-07 21:22:09 +0300

    complete balls

--- Train1/balls.cpp
+++ Train1/balls.cpp
@@ -9,11 +9,9 @@
 using namespace std;
 
 void balls(){
-    freopen("balls.in", "r", stdin);
     int t;
     scanf("%d", &t);
     vector<int> tests(t);
-    vector<int> answers(t);
 
     int m;
     for (int i = 0; i < t; ++i){
@@ -21,9 +19,35 @@ void balls(){
         tests[i] = m;
     }
 
-    freopen("balls.out", "w", stdout);
-    for (int a: answers)
-        printf("%d \n", a);
+    vector<int> ballsInLayer;
+    vector<int> ballsInPyr;
+
+    ballsInLayer.push_back(1);
+    ballsInPyr.push_back(1);
+    int i = 1;
+    while (true){
+        ++i;
+        ballsInLayer.push_back(ballsInLayer[i-2] + i);
+        int cntBalls = ballsInPyr[i-2] + ballsInLayer[i-1];
+        if (cntBalls > 300000)
+            break;
+        ballsInPyr.push_back(cntBalls);
+    }
+
+    vector<int> cntPyramid(300000+1, INT32_MAX);
+    for (int b: ballsInPyr){
+        cntPyramid[b] = 1;
+    }
+    for (int i = 1; i <= 300000; ++i){
+        for (int b: ballsInPyr){
+            if (i - b <= 0)
+                break;
+            cntPyramid[i] = min(cntPyramid[i], cntPyramid[i-b]+1);
+        }
+    }
+
+    for (int t: tests)
+        printf("%d \n", cntPyramid[t]);
 }
 
 int main(){
--- Train1/peacefulsets.cpp
+++ Train1/peacefulsets.cpp
@@ -1,4 +1,28 @@
 //
 // Created by Ilya on 06.09.2018.
 //
+#include <iostream>
+#include <stdio.h>
+#include <vector>
 
+using namespace std;
+
+void peacefulsets(){
+    int n;
+    scanf("%d", &n);
+    vector<vector<int>> weights(n, vector<int>(n, 0));
+
+
+    for (int i = n-2; i >= 0; --i){
+        for (int j = 0; j < i+1; ++j){
+            weights[i][j] += max(weights[i+1][j], weights[i+1][j+1]);
+        }
+    }
+
+    printf("%d ", weights[0][0]);
+}
+
+int main(){
+    peacefulsets();
+    return 0;
+}
