9cc07a61d3747ee2ecf59731f996264aa4928c22
CodeIsLie <ilya1757@gmail.com>
2018-09-23 21:15:42 +0300

    start work on G

--- Train1/round502/main.cpp
+++ Train1/round502/main.cpp
@@ -1,5 +1,3 @@
-#define FILE2 "sum2"
-
 #include <set>
 #include <vector>
 #include <iostream>
@@ -18,10 +16,6 @@ int main(){
     freopen("./inp.txt", "r", stdin);
     freopen("./out.txt", "w", stdout);
 #endif
-#ifdef FILE2
-    freopen(FILE2".in", "r", stdin);
-    freopen(FILE2".out", "w", stdout);
-#endif
     task();
 
 #ifdef TEAM
@@ -31,50 +25,39 @@ int main(){
     return 0;
 }
 
-vector<int> a;
-vector<long long> tsum;
 
-void build (int v, int tl, int tr) {
-    if (tl == tr) {
-        tsum[v] = a[tl];
-    }
-    else {
-        int tm = (tl + tr) / 2;
-        build (v*2, tl, tm);
-        build (v*2+1, tm+1, tr);
-        tsum[v] = tsum[v*2] + tsum[v*2+1];
-    }
-}
-
-long long sum (int v, int tl, int tr, int l, int r) {
-    if (l > r)
-        return 0;
-    if (l == tl && r == tr)
-        return tsum[v];
-    int tm = (tl + tr) / 2;
-    return sum (v*2, tl, tm, l, min(r,tm)) + sum (v*2+1, tm+1, tr, max(l,tm+1), r);
-}
 
 
 void task(){
-    int n;
-    scanf("%d", &n);
+    int n, m, k;
+    cin >> n;
 
-    a.assign(n, 0);
-    int num;
+    vector<int> ps;
+    ps.assign(n, 0);
     for (int i = 0; i < n; ++i){
-        scanf("%d", &num);
-        a[i]=num;
+        cin >> ps[i];
     }
 
-    tsum.assign(4*n, 0);
-    build(1, 0, n-1);
-
-    int k;
-    scanf("%d", &k);
-    int l, r;
-    for (int i = 0; i < k; ++i){
-        scanf("%d %d", &l, &r);
-        printf("%lld\n", sum(1, 0, n-1, l-1, r-1));
+    sort(ps.rbegin(), ps.rend());
+    map<int, int> isPs;
+    isPs[1]
+    long long mult = 1;
+    bool add1= false;
+    for (int p: ps){
+        if (!isPs[p]) {
+            isPs[p] = 1;
+            mult = mult*p % 1000000007;
+        }else
+        if (isPs[p] == 1) {
+            isPs[p] = 2;
+            if (mult % (p-1) != 0)
+                mult = mult*(p-1) % 1000000007;
+            isPs[p-1] = 1;
+        }else{
+            add1 = true;
+        }
     }
+    mult += (int)add1;
+
+    cout << mult;
 }
