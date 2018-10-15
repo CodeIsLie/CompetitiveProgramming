453061c8bd465a8bb4e1c3f861f64a96169a3053
CodeIsLie <ilya1757@gmail.com>
2018-10-15 10:51:02 +0300

    complete graphs

--- Train1/round502/main.cpp
+++ Train1/round502/main.cpp
@@ -36,24 +36,107 @@ struct Edge{
     int to;
     int ind;
     int type;
+    int start;
 };
+
+
+vector<vector<Edge>> graph;
+vector<char> used;
+map<int, char> orientation;
+
+int minCntVertices = 0;
+int maxCntVertices = 0;
+
+void minDFS(int v){
+    used[v] = 1;
+    minCntVertices++;
+    for (auto edge: graph[v]){
+        if (edge.type == 1 ){
+            if (!used[edge.to])
+                minDFS(edge.to);
+        }else{
+            if (orientation[edge.ind] == 0){
+                if (edge.start == edge.to){
+                    orientation[edge.ind] = '+';
+                }
+                else{
+                    orientation[edge.ind] = '-';
+                }
+            }
+        }
+    }
+}
+
+void maxDFS(int v){
+    used[v] = 1;
+    maxCntVertices++;
+    for (auto edge: graph[v]){
+        if (edge.type == 1 ){
+            if (!used[edge.to])
+                maxDFS(edge.to);
+        }else{
+            if (orientation[edge.ind] == 0){
+                if (edge.start == edge.to){
+                    orientation[edge.ind] = '-';
+                }
+                else{
+                    orientation[edge.ind] = '+';
+                }
+                if (!used[edge.to])
+                    maxDFS(edge.to);
+            }
+        }
+    }
+}
 
 void task() {
     int n, m, s;
     cin >> n >> m >> s;
 
-    vector<vector<Edge>> graph;
-    graph.resize(n);
-    map<int, char> orientation;
+    graph.resize(n+1);
+    used.assign(n+1, 0);
     int t, u ,v;
     for (int i = 0; i < m; ++i){
         cin >> t >> u >> v;
         if ( t == 1 ){
-            graph[u].push_back({v, i, 1});
+            graph[u].push_back({v, i, 1, u});
         }else{
             orientation[i] = 0;
-            graph[u].push_back({v, i, 2});
-            graph[v].push_back({u, i, 2});
+            graph[u].push_back({v, i, 2, u});
+            graph[v].push_back({u, i, 2, u});
         }
     }
+    maxDFS(s);
+    for (auto& edge: orientation){
+        if (edge.second == 0){
+            edge.second = '+';
+        }
+    }
+
+    used.assign(n+1, 0);
+    string orient = "";
+    for (auto it = orientation.begin(); it != orientation.end(); ++it){
+        orient += string(1, it->second);
+        it -> second = 0;
+    }
+
+    cout << maxCntVertices << endl;
+    cout << orient << endl;
+
+    minDFS(s);
+    orient = "";
+
+    for (auto& edge: orientation){
+        if (edge.second == 0){
+            edge.second = '+';
+        }
+    }
+
+    for (auto it = orientation.begin(); it != orientation.end(); ++it){
+        orient += string(1, it->second);
+        it -> second = 0;
+    }
+
+    cout << minCntVertices << endl;
+    cout << orient << endl;
 }
