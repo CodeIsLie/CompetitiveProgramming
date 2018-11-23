4e5bb9dd3c03e47b56d627c8c7f505b90841194c
CodeIsLie <ilya1757@gmail.com>
2018-11-24 00:47:17 +0300

    round 523 task 3

--- Train1/round502/main.cpp
+++ Train1/round502/main.cpp
@@ -33,26 +33,40 @@ int main(){
 }
 
 void task(){
-    int n, m;
-    cin >> n >> m;
-    multiset<int> heights;
-    long long all = 0;
-    int h;
-    for (int i = 0; i < n; ++i){
-        cin >> h;
-        heights.insert(h);
-        all += h;
+    int n;
+    cin >> n;
+    vector<long long> a(n);
+    int max_n = 0;
+    for (auto& el: a){
+        cin >> el;
+        max_n = max(max_n, (int)el);
     }
+    long long lnum = 1000000007;
 
-    vector<int> hs = vector<int>(heights.rbegin(), heights.rend());
-    int dels = 0;
-    int cur = min(m, hs[0]);
-    for (int i = 1; i < hs.size(); ++i){
-        dels = max(1, cur - min(cur, hs[i]));
-        all -= dels;
-        cur = cur - dels;
+    vector<long long> divisors(max_n+1, 0);
+    divisors[1] = 1;
+    long long cnt_divs = 0;
+    for (int i = 1; i < n; ++i){
+        vector<long long> inc_n;
+        for (int j = 1; j*j <= min((long long)(i+1)*(i+1), a[i]); ++j){
+            if (a[i] % j == 0){
+//                cnt_divs = (cnt_divs + divisors[j-1]) % lnum;
+//                if (j != a[i]/j && a[i]/j <= i+1)
+//                    cnt_divs = (cnt_divs + divisors[a[i]/j-1]) % lnum;
+                inc_n.push_back(j);
+                if (a[i]/j <= i+1 && a[i]/j != j)
+                    inc_n.push_back(a[i]/j);
+            }
+        }
+        sort(inc_n.begin(), inc_n.end());
+        reverse(inc_n.begin(), inc_n.end());
+        for (auto& inc: inc_n)
+            divisors[inc]= (divisors[inc] + divisors[inc-1]) % lnum;
+        divisors[1]++;
     }
-    dels = max(1, cur);
-    all -= dels;
-    cout << all << endl;
+    for (auto div: divisors){
+        cnt_divs += div;
+    }
+    cnt_divs = cnt_divs % lnum;
+    cout << cnt_divs << endl;
 }
