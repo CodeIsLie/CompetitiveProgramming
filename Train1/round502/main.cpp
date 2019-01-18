c6f89b763e090213234b2034e33ed3e2169dd020
Ilya Yakubovsky <ilya1757@gmail.com>
2019-01-18 22:55:48 +0300

    Codeforces #526 task 4 complete

--- Train1/round502/main.cpp
+++ Train1/round502/main.cpp
@@ -35,33 +35,50 @@ int main(){
 vector<vector<pair<int, int>>> graph;
 vector<char> used;
 vector<int> fuels;
-vector<long long> paths;
+vector<map<int, long long>> paths;
 
 void DFS(int v){
     used[v] = 1;
 
     for (auto edge: graph[v]){
         int to = edge.first;
+        long long weight = edge.second;
         if (!used[to]){
             DFS(to);
-        }
-    }
 
-    long long max_path = 0;
-    for (auto edge: graph[v]){
-        int to = edge.first;
-        long long weight = edge.second;
-        if (fuels[to] + weight > max_path){
-            max_path = fuels[to] + weight;
+            long long path_len = (paths[to].empty()) ?
+                                 fuels[to] :
+                                 max_element(paths[to].begin(), paths[to].end(),
+                                             [](const pair<int, long long> p1, const pair<int, long long> p2){
+                                                 return p1.second < p2.second;
+                                             })->second;
+            if (path_len + weight > 0){
+                paths[v][to] = fuels[v] + weight + path_len;
+            }
         }
     }
-    paths[v] = fuels[v] + max_path;
 }
 
 long long find_max_path(int n){
     long long max_path = fuels[0];
     // find 2 maximal paths
+
     for (int i = 0; i < n; ++i){
+        vector<long long> path_lens;
+        for (auto &e: paths[i]){
+            path_lens.push_back(e.second);
+        }
+        sort(path_lens.rbegin(), path_lens.rend());
+        long long current_path = 0;
+        if (path_lens.empty()){
+            current_path = fuels[i];
+        }
+        else if (path_lens.size() < 2){
+            current_path = path_lens[0];
+        }else{
+            current_path = -fuels[i] + path_lens[0] + path_lens[1];
+        }
+        max_path = max(max_path, current_path);
 
     }
     return max_path;
@@ -75,14 +92,17 @@ void task(){
         cin >> f;
     }
 
+    graph.resize(n);
     int u, v, c;
-    for (int i = 0; i < n; ++i){
+    for (int i = 0; i < n-1; ++i){
         cin >> u >> v >> c;
         graph[u-1].emplace_back(v-1, -c);
         graph[v-1].emplace_back(u-1, -c);
     }
 
-    paths.assign(n, 0);
+    paths.resize(n);
     used.assign(n, 0);
     DFS(0);
+
+    cout << find_max_path(n) << endl;
 }
